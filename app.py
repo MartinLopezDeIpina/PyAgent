08e690b91c6a154b8995e320b50fef945ebc9f33
MartinLopezDeIpina <martinsaski@gmail.com>
2025-02-13 22:04:12 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from routes.docs import docs_bp
+from config import Config
+from models import db
+
+app = Flask(__name__)
+
+app.register_blueprint(docs_bp, url_prefix="/docs")
+app.config.from_object(Config)
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run()
